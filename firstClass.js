3b4f01095e6c5897639bc213ddb42158015427c0
noel-n <mhc390nwaghodoh@meadowschool.org>
2020-02-21 10:16:14 +0100

    my first class learning javascript and git

---
+++ firstClass.js
@@ -0,0 +1,9 @@
+ //window.alert("Hello World");
+
+ const firstArray = new Array(5);
+ console.log("hello world1");
+ for (i=0; i<firstArray.length; i++)
+ {
+    console.log(i);
+ }
+ console.log("hello world2");
